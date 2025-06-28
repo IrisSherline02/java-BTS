012f5c7a9669951d3862ba6992d26a47be2d5a61
IrisSherline02 <irissherlinegladis@gmail.com>
2025-06-28 11:13:24 +0530

    Add files via upload

---
+++ Number1.java
@@ -0,0 +1,12 @@
+class Number1
+{
+public static void main(String[] args)
+{
+int a=0;
+do
+{
+ System.out.println(a);
+a++;
+}while(a<=100);
+}
+}
---
+++ Numbers.java
@@ -0,0 +1,14 @@
+class Numbers
+{
+public static void main(String[] args)
+{
+   int a=100;
+  
+do
+{
+   System.out.println(a);
+   a--;
+
+}while(a>=0);
+}
+}
